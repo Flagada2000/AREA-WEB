83c14ad9bacfa091efed2d7a9a4dba213a4283d6
gaetan.darrort <gaetan.darrort@epitech.eu>
2024-01-08 04:10:23 +0100

    Added : implementation of user actions

--- src/app/(site)/myactions/page.tsx
+++ src/app/(site)/myactions/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import "./myactions.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
-import { Action, User, UserAction } from "@/types/types";
+import {
+  ActionUserInfo,
+  User,
+} from "@/types/types";
 import APIService from "../../../service/service";
 import { useRouter } from "next/navigation";
 import MyActionCard from "@/components/MyActionCard";
@@ -12,8 +15,7 @@ import MyExplorerCard from "@/components/MyExplorerCard";
 export default function Home() {
   const apiService = new APIService();
   const [user, setUser] = useState<User | null>();
-  const [actions, setActions] = useState<Action[] | null>();
-  const [userActions, setUserActions] = useState<UserAction[] | null>();
+  const [actions, setActions] = useState<ActionUserInfo[]>([]);
   const router = useRouter();
 
   const fetchUserData = async () => {
@@ -30,48 +32,19 @@ export default function Home() {
     }
   };
 
-  const fetchUserActions = async () => {
-    const hardcodedUserActions: UserAction[] = [
-      {
-        id: 1,
-        action_id: 1,
-        user_id: "1",
-        action_config: "ttest",
-      },
-      {
-        id: 2,
-        action_id: 2,
-        user_id: "1",
-        action_config: "ttest",
-      },
-    ];
-
-    const hardcodedActions: Action[] = [
-        {
-            id: 1,
-            action_name: "Nouveau caca",
-            service_id: 1,
-        },
-        {
-            id: 2,
-            action_name: "Nouveau caca",
-            service_id: 1,
-        },
-        ];
-
-    setActions(hardcodedActions);
-    setUserActions(hardcodedUserActions);
+  const fetchActions = async () => {
+    try {
+      const actionsUserInfos = await apiService.getUserActionInfo();
+      if (actionsUserInfos) setActions(actionsUserInfos);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
     fetchUserData();
-    fetchUserActions();
+    fetchActions();
   }, []);
-
-  useEffect(() => {
-    console.log(user);
-    console.log(user?.email);
-  }, [user]);
 
   return (
     <div className="w-full h-full bg-slate-900 rounded-[30px] flex-col justify-start items-center inline-flex">
@@ -93,76 +66,11 @@ export default function Home() {
         {/* ligne de "mes actions" */}
         <div className="scroll-container">
           <div className="justify-start items-center gap-[30px] inline-flex">
-            {/* 1er carte action */}
-            <div className="h-[319px] w-[319px] justify-center items-center gap-2.5 flex">
-              <div className="grow basis-0 self-stretch p-[30px] bg-blue-100 rounded-[30px] flex-col justify-center items-center gap-[30px] inline-flex">
-                <div className="self-stretch justify-between items-center inline-flex">
-                  <button className="group w-[30px] h-[30px] relative">
-                    <FontAwesomeIcon
-                      icon={faTrash}
-                      className="text-gray-400 transform group-hover:text-blue-400"
-                    />
-                  </button>
-                  <label className="switch">
-                    <input type="checkbox" />
-                    <span className="slider"></span>
-                  </label>
-                </div>
-                <div className="self-stretch text-blue-900 text-3xl font-bold font-inter">
-                  Nouveau tweet
-                </div>
-                <div className="self-stretch h-[0px] border border-blue-900"></div>
-                <div className="self-stretch justify-start items-center inline-flex">
-                  <div className="justify-center items-center gap-[15px] flex">
-                    <img
-                      className="w-[50px] h-[50px] rounded-[5px]"
-                      src="https://via.placeholder.com/50x50"
-                    />
-                    <img
-                      className="w-[50px] h-[50px] rounded-[5px]"
-                      src="https://via.placeholder.com/50x50"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            {/* 2 eme carte action */}
-            <div className="h-[319px] w-[319px] justify-center items-center gap-2.5 flex">
-              <div className="grow basis-0 self-stretch p-[30px] bg-blue-100 rounded-[30px] flex-col justify-center items-center gap-[30px] inline-flex">
-                <div className="self-stretch justify-between items-center inline-flex">
-                  <button className="group w-[30px] h-[30px] relative">
-                    <FontAwesomeIcon
-                      icon={faTrash}
-                      className="text-gray-400 transform group-hover:text-blue-400"
-                    />
-                  </button>
-                  <label className="switch">
-                    <input type="checkbox" />
-                    <span className="slider"></span>
-                  </label>
-                </div>
-                <div className="self-stretch text-blue-900 text-3xl font-bold font-inter">
-                  Nouveau tweet
-                </div>
-                <div className="self-stretch h-[0px] border border-blue-900"></div>
-                <div className="self-stretch justify-start items-center inline-flex">
-                  <div className="justify-center items-center gap-[15px] flex">
-                    <img
-                      className="w-[50px] h-[50px] rounded-[5px]"
-                      src="https://via.placeholder.com/50x50"
-                    />
-                    <img
-                      className="w-[50px] h-[50px] rounded-[5px]"
-                      src="https://via.placeholder.com/50x50"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
+            {actions.map((actionInfo, index) => (
+              <MyActionCard key={index} action={actionInfo} />
+            ))}
 
-            {/* 3eme carte action */}
-            {userActions && actions ? <MyActionCard action={actions[0]} userAction={userActions[0]} /> : "Loading..."}
           </div>
         </div>
 
